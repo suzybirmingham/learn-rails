af967fd30984028861b5f42e4eafc19754135b47
suzy birmingham <suzy@suzybirmingham.com>
2015-10-19 09:08:24 -0700

    adjust settings for sending email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,14 +29,15 @@ Rails.application.configure do
 
   # to configure Rails to send emails
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: Rails.application.secrets.domain_name,
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: Rails.application.secrets.email_provider_username,
-  password: Rails.application.secrets.email_provider_password
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
   }
+
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
